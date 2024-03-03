77a2dd44691e347ba3aefe1a2bf3e9491226fbbc
Ricard Dalmau <rdalmau@gmail.com>
2024-03-03 23:59:34 +0100

    clarity and google analytic keys as env vars

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+  const cKey : string = process.env.REACT_APP_CLARITY_ID;
+  const clarityKey : string =  `
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+   })(window, document, "clarity", "script","`.concat(cKey, "\");");
+ 
+   //console.log("clarityKey: ", clarityKey);
+
   return (
     <html lang="en">
       { /* Microsoft Clarity tag */ }
@@ -20,19 +31,14 @@ export default function RootLayout({
         id="clarity2"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
-          __html: `
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "lau5rlq9k8");`,
+          __html: clarityKey,
         }}
       />
       <body>
         <JotaiProvider>{children}</JotaiProvider>
       </body>
       { /* Google Analytics tag */ }
-      <GoogleAnalytics  gaId ="G-4GMMLPGZ7Q" />
+      <GoogleAnalytics  gaId = {process.env.REACT_APP_GOOGLE_GA_ID} />
     </html>
   );
 }
