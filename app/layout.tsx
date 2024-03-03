8b76b4771892c3ebdb60044a963d099f77492018
Ricard Dalmau <rdalmau@gmail.com>
2024-03-04 00:11:17 +0100

    avoid undefined key warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,13 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
 
-  const cKey : string = process.env.REACT_APP_CLARITY_ID;
+  const cKeyString : string = process.env.REACT_APP_CLARITY_ID !== undefined ? process.env.REACT_APP_CLARITY_ID : "";
+  
   const clarityKey : string =  `
   (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-   })(window, document, "clarity", "script","`.concat(cKey, "\");");
+   })(window, document, "clarity", "script","`.concat(cKeyString, "\");");
  
    //console.log("clarityKey: ", clarityKey);
 
@@ -38,7 +39,7 @@ export default function RootLayout({
         <JotaiProvider>{children}</JotaiProvider>
       </body>
       { /* Google Analytics tag */ }
-      <GoogleAnalytics  gaId = {process.env.REACT_APP_GOOGLE_GA_ID} />
+      <GoogleAnalytics  gaId = {process.env.REACT_APP_GOOGLE_GA_ID !== undefined ? process.env.REACT_APP_GOOGLE_GA_ID : ""}  />
     </html>
   );
 }
