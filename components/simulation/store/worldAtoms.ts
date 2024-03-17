e510bb137cab61c72a49aa78101abdaa122e13b9
Ricard Dalmau <rdalmau@gmail.com>
2024-03-17 01:09:40 +0100

    change default map to vertical bins

--- components/simulation/store/worldAtoms.ts
+++ components/simulation/store/worldAtoms.ts
@@ -62,16 +62,17 @@ export const worldInitialValuesAtom = atom((get) => ({
 
 // Initial settings
 export const worldObjectsAtom = atom<WorldObject[]>([
-    // A reproduction zone at the center
-    new RectangleReproductionArea(0.25, 0.25, 0.5, 0.5, true),
-    // A map divided in two sections by 5 squares
-    new RectangleObject(0, 0, 0.2, 0.2),
-    new RectangleObject(0.2, 0.2, 0.2, 0.2),
-    new RectangleObject(0.4, 0.4, 0.2, 0.2),
-    new RectangleObject(0.6, 0.6, 0.2, 0.2),
-    new RectangleObject(0.8, 0.8, 0.2, 0.2),
-    // A spawn zone at the center
-    new RectangleSpawnArea(0.4, 0.4, 0.2, 0.2, true),
+    // A spawn zone at top left
+    new RectangleSpawnArea(0.1, 0.1, 0.2, 0.2, true),
+    // A reproduction zone at  center
+    new RectangleReproductionArea(0.3, 0.6, 0.2, 0.4, true),
+    // A map divided at bottom by 5 columns
+    new RectangleObject(0.1, 0.6, 0.04, 0.4),
+    new RectangleObject(0.3, 0.6, 0.04, 0.4),
+    new RectangleObject(0.5, 0.6, 0.04, 0.4),
+    new RectangleObject(0.7, 0.6, 0.04, 0.4),
+    new RectangleObject(0.9, 0.6, 0.04, 0.4),
+    
 ]);
 
 
