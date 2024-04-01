6f0cc58bc92308ad4f83ee81d9473fa0292f7fa7
Ricard Dalmau <rdalmau@gmail.com>
2024-04-01 22:45:29 +0200

    creation constants file

--- components/simulation/store/worldAtoms.ts
+++ components/simulation/store/worldAtoms.ts
@@ -1,73 +1,33 @@
-import { ActionName } from "@/simulation/creature/actions/CreatureActions";
-import { MutationMode } from "@/simulation/creature/genome/MutationMode";
-import { SensorName } from "@/simulation/creature/sensors/CreatureSensors";
 import World from "@/simulation/world/World";
-import WorldObject from "@/simulation/world/WorldObject";
 import { atom } from "jotai";
-import SelectionMethod from "@/simulation/creature/selection/SelectionMethod";
-import InsideReproductionAreaSelection from "@/simulation/creature/selection/InsideReproductionAreaSelection";
-import PopulationStrategy from "@/simulation/creature/population/PopulationStrategy";
-import AsexualZonePopulation from "@/simulation/creature/population/AsexualZonePopulation";
-import RectangleReproductionArea from "@/simulation/world/areas/reproduction/RectangleReproductionArea";
-import RectangleSpawnArea from "@/simulation/world/areas/spawn/RectangleSpawnArea";
-import RectangleObject from "@/simulation/world/objects/RectangleObject";
+import * as constants from "@/simulation/simulationConstants"
 
 // Controller
 export const worldAtom = atom<World | null>(null);
 export const restartAtom = atom(false);
-export const restartCountAtom = atom(0);  // refresh tabs on restart
+export const restartCountAtom = atom(0);  // to refresh tabs on restart
 
 // Run parameters
-export const sizeAtom = atom(100);
-export const stepsPerGenAtom = atom(300);
-export const initialPopulationAtom = atom(500);
+export const sizeAtom = atom(constants.RUN_WORLD_SIZE);
+export const stepsPerGenAtom = atom(constants.RUN_STEPS_PER_GENERATION);
+export const initialPopulationAtom = atom(constants.RUN_INITIAL_POPULATION);
 
 // Simulation parameters
-export const selectionMethodAtom = atom<SelectionMethod>(new InsideReproductionAreaSelection());
-export const populationStrategyAtom = atom<PopulationStrategy>(new AsexualZonePopulation());
-export const initialGenomeSizeAtom = atom(4);
-export const maxGenomeSizeAtom = atom(30);
-export const maxNumberNeuronsAtom = atom(15);
-export const mutationModeAtom = atom<MutationMode>(MutationMode.wholeGene);
-export const mutationProbabilityAtom = atom(0.05);
-export const geneInsertionDeletionProbabilityAtom = atom(0.015);
+export const selectionMethodAtom = atom(constants.RUN_SELECTION_METHOD);
+export const populationStrategyAtom = atom(constants.RUN_POPULATION_STRATEGY);
+export const initialGenomeSizeAtom = atom(constants.RUN_INITIAL_GENOME_SIZE);
+export const maxGenomeSizeAtom = atom(constants.RUN_MAX_GENOME_SIZE);
+export const maxNumberNeuronsAtom = atom(constants.RUN_MAX_NUMBER_NEURONS);
+export const mutationModeAtom = atom(constants.RUN_MUTATION_MODE);
+export const mutationProbabilityAtom = atom(constants.RUN_MUTATION_PROBABILITY);
+export const geneInsertionDeletionProbabilityAtom = atom(constants.RUN_GENE_INSERTION_DELETION_PROBABILITY);
 
 // Creatures
-export const enabledSensorsAtom = atom<SensorName[]>([
-  "HorizontalPosition",
-  "VerticalPosition",
-  "Age",
-  "Oscillator",
-  "Random",
-  "HorizontalSpeed",
-  "VerticalSpeed",
-  "HorizontalBorderDistance",
-  "VerticalBorderDistance",
-  "BorderDistance",
-]);
-export const enabledActionsAtom = atom<ActionName[]>([
-  "MoveNorth",
-  "MoveSouth",
-  "MoveEast",
-  "MoveWest",
-  "RandomMove",
-  "MoveForward",
-]);
+export const enabledSensorsAtom = atom(constants.RUN_ENABLED_SENSORS);
+export const enabledActionsAtom = atom(constants.RUN_ENABLED_ACTIONS);
 
 // Map objects
-export const worldObjectsAtom = atom<WorldObject[]>([
-  // A spawn zone at top left
-  new RectangleSpawnArea(0.1, 0.1, 0.2, 0.2, true),
-  // A reproduction zone at  center
-  new RectangleReproductionArea(0.3, 0.6, 0.2, 0.4, true),
-  // A map divided at bottom by 5 columns
-  new RectangleObject(0.1, 0.6, 0.04, 0.4),
-  new RectangleObject(0.3, 0.6, 0.04, 0.4),
-  new RectangleObject(0.5, 0.6, 0.04, 0.4),
-  new RectangleObject(0.7, 0.6, 0.04, 0.4),
-  new RectangleObject(0.9, 0.6, 0.04, 0.4),
-  
-]);
+export const worldObjectsAtom = atom(constants.RUN_WORLD_OBJECTS);
 
 
 // Initial settings bundle
---
+++ simulation/simulationConstants.ts
@@ -0,0 +1,87 @@
+
+import { SensorName } from "@/simulation/creature/sensors/CreatureSensors";
+import { ActionName } from "@/simulation/creature/actions/CreatureActions";
+import SelectionMethod from "@/simulation/creature/selection/SelectionMethod";
+import InsideReproductionAreaSelection from "@/simulation/creature/selection/InsideReproductionAreaSelection";
+import PopulationStrategy from "@/simulation/creature/population/PopulationStrategy";
+import AsexualZonePopulation from "@/simulation/creature/population/AsexualZonePopulation";
+import ReproductionSelection from "@/simulation/creature/selection/ReproductionSelection";
+import { MutationMode } from "@/simulation/creature/genome/MutationMode";
+import RectangleReproductionArea from "@/simulation/world/areas/reproduction/RectangleReproductionArea";
+import RectangleSpawnArea from "@/simulation/world/areas/spawn/RectangleSpawnArea";
+import WorldObject from "@/simulation/world/WorldObject";
+import RectangleObject from "@/simulation/world/objects/RectangleObject";
+
+export const WATER_GRIDPOINT_DEFAULT = 1;
+export const ENERGY_GRIDPOINT_DEFAULT = 0.01;
+export const WATER_TO_MASS_PER_STEP = 0.1;
+// --> to do future: mass loss depends on creature complexity - genome size
+export const MASS_LOSS_PER_STEP = 0.05
+export const CREATURE_MASS_GENERATION_0 = 1;
+
+// --> to be calculated from the creature's grid real value
+export const TEMP_ENERGY_CELL_CREATURE = 0.01;
+export const TEMP_WATER_CELL_CREATURE = 1;
+
+export const DEBUG_CREATURE_ID : number = 1;   // if -1, no debug, if 0 all creatures, else a id 
+
+// selection methods
+export const GREATEST_DISTANCE_SELECTION_TOP_SURVIVORS = 0.05; 
+
+export const colors = {
+    reproduction: "rgba(0,0,255,0.1)",
+    obstacle: "rgb(60, 60, 60)",
+    healing: "rgba(0,255,0, 0.1)",
+    danger: "rgba(255,0,0, 0.1)",
+    spawn: "rgba(255, 255, 0, 0.1)",
+  
+  };
+  
+// run parameters
+export const RUN_WORLD_SIZE = 100;
+export const RUN_STEPS_PER_GENERATION = 300;
+export const RUN_INITIAL_POPULATION = 500;
+export const RUN_INITIAL_GENOME_SIZE = 4;
+export const RUN_MAX_GENOME_SIZE = 30;
+export const RUN_MAX_NUMBER_NEURONS = 15;
+export const RUN_MUTATION_PROBABILITY = 0.05;
+export const RUN_GENE_INSERTION_DELETION_PROBABILITY = 0.015;
+export const RUN_ENABLED_SENSORS : SensorName[]= [
+    "HorizontalPosition",
+    "VerticalPosition",
+    "Age",
+    "Oscillator",
+    "Random",
+    "HorizontalSpeed",
+    "VerticalSpeed",
+    "HorizontalBorderDistance",
+    "VerticalBorderDistance",
+    "BorderDistance",
+    "Mass"
+  ];
+export const RUN_ENABLED_ACTIONS : ActionName[] = [
+    //  "MoveNorth",
+    //  "MoveSouth",
+    //  "MoveEast",
+    //  "MoveWest",
+    //  "RandomMove",
+    //  "MoveForward",
+      "Photosynthesis",
+      "Reproduction"
+    ];
+export const RUN_SELECTION_METHOD : SelectionMethod = new ReproductionSelection();
+export const RUN_POPULATION_STRATEGY : PopulationStrategy = new AsexualZonePopulation();
+export const RUN_MUTATION_MODE : MutationMode = MutationMode.wholeGene;
+
+export const RUN_WORLD_OBJECTS : WorldObject[] = [
+    // A spawn zone at top left
+    new RectangleSpawnArea(0.1, 0.1, 0.2, 0.2, true),
+    // A reproduction zone at  center
+    new RectangleReproductionArea(0.3, 0.6, 0.2, 0.4, true),
+    // A map divided at bottom by 5 columns
+    new RectangleObject(0.1, 0.6, 0.04, 0.4),
+    new RectangleObject(0.3, 0.6, 0.04, 0.4),
+    new RectangleObject(0.5, 0.6, 0.04, 0.4),
+    new RectangleObject(0.7, 0.6, 0.04, 0.4),
+    new RectangleObject(0.9, 0.6, 0.04, 0.4),
+  ];
