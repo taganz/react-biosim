bb534bf8548b6d8cd726fdf767a9b90bb736680e
Ricard Dalmau <rdalmau@gmail.com>
2024-04-20 16:22:19 +0200

    refactor: rename file

---
+++ simulation/generations/WorldGenerationsData.ts
@@ -0,0 +1,27 @@
+import  {SensorName}  from "@/simulation/creature/brain/CreatureSensors";
+import  {ActionName} from "@/simulation/creature/brain/CreatureActions";
+import SelectionMethod from "@/simulation/generations/selection/SelectionMethod";
+import PopulationStrategy from "@/simulation/generations/population/PopulationStrategy";
+import { MutationMode } from "@/simulation/creature/brain/MutationMode";
+
+export default interface WorldGenerationsData {
+    // initial values
+    populationStrategy: PopulationStrategy;
+    selectionMethod: SelectionMethod;
+    initialPopulation: number;
+    initialGenomeSize: number;
+    maxGenomeSize: number;
+    maxNumberNeurons: number;
+    mutationMode: MutationMode;
+    mutationProbability: number;
+    deletionRatio: number;
+    geneInsertionDeletionProbability: number;
+    enabledSensors: SensorName[];
+    enabledActions: ActionName[];
+    // state values 
+    lastCreatureIdCreated: number;
+    lastCreatureCount: number;
+    lastSurvivorsCount: number;
+    lastFitnessMaxValue: number;
+    lastSurvivalRate: number;
+}
