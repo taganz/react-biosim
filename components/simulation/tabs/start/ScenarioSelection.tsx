c5696b1d854843efb85fbf80288dbe800d3ed419
Ricard Dalmau <rdalmau@gmail.com>
2024-04-26 18:09:07 +0200

    fix

--- components/simulation/tabs/start/ScenarioSelection.tsx
+++ components/simulation/tabs/start/ScenarioSelection.tsx
@@ -7,35 +7,18 @@ import {scenarioObjects, ScenarioObjects} from "./scenarioObjects";
 import {loadSavedWorldAndStartRun } from "@/simulation/serialization/loadWorld";
 import SavedWorld from '@/simulation/serialization/data/SavedWorld';
 
+  
 export default function ScenariosSelection () {
     
   const worldController = useAtomValue(worldControllerAtom);
-  const worldControllerSimCode = atom(worldController?.simCode);
   const setWorldControllerData = useSetAtom(worldControllerDataAtom);
   const setWorldGenerationData = useSetAtom(worldGenerationDataAtom);
   
 
   const scenariosOptions: Option[] = Array.from({ length: scenarioObjects.length }, (_, i) => ({ value: i.toString(), label: scenarioObjects[i].name }));
 
-/*
-  const handleSelection = (value: string) => {
-    if (worldController) {
-      const parsedScenario = scenarioObjects[parseInt(value)].data;
-      //const data = parsedScenario.toString().concat('"lastCreatureIdCreated":0,"lastCreatureCount":0,"lastSurvivorsCount":0,"lastSurvivalRate":0,"lastGenerationDuration":0,"totalTime":0,"species":[], "generations":{"generations":[],"maxSurvivorCount":0,"minSurvivorCount":0,"maxFitnessValue":0');
-      
-      //TODO revisar el "as unknown"!
-      const [readWorldControllerData, readWorldGenerationData]  = loadSavedWorldAndStartRun(worldController, parsedScenario as string);
-      setWorldGenerationData(readWorldGenerationData);
-      setWorldControllerData(readWorldControllerData);
-      
-    }
-    else {
-      console.warn("ScenariosSelection worldController not found!");
-    }
-  }
-*/
   // functionloadScenario
-  const handleSelection2 = async (value: string) => {
+  const handleSelection = async (value: string) => {
     if (worldController) {
         try {
           const filename = scenarioObjects[parseInt(value)].filename;
@@ -59,8 +42,7 @@ export default function ScenariosSelection () {
 return (
   <div className="mb-1">
     <h2>Select simulation scenario: </h2>
-    <Dropdown options={scenariosOptions} onSelect={handleSelection2} />
-    Current simulation: {worldController?.simCode}
+    <Dropdown options={scenariosOptions} onSelect={handleSelection} />
     <br/>
   </div>
 );
