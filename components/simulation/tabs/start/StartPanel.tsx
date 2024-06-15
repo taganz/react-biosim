8416198cd7d211029e7afdbef53b0cac81e2d063
Ricard Dalmau <rdalmau@gmail.com>
2024-06-15 10:34:06 +0200

    fix and ux

--- components/simulation/tabs/start/StartPanel.tsx
+++ components/simulation/tabs/start/StartPanel.tsx
@@ -9,16 +9,14 @@ export default function StartPanel() {
   return (
     <div>
       <p className="mb-2 text-lg">
-        This is an evolution simulator. Observe random created creatures evolving across generations.  
+        This is an evolution simulator to observe random created creatures evolving across generations.  
         <br/><br/>
-        Start by selecting a scenario. 
-      </p>
-      <div className="text-lg">
-      <ScenariosSelection/>
-      </div>
+        In the first scenario creatures &quot;learn&quot; how to move to the darker right area. 
+        <br/><br/>
+        Try other scenarios and modify them using &quot;Settings&quot;panel: 
+        <ScenariosSelection/>
+        </p>
       <p className="mb-2 text-lg">
-        Create your own scenario using Settings and Map tabs.
-        <br/><br/>
         See &quot;About&quot; tab for more info and link to David R. Miller inspiring video.
       </p>
     </div>
